2195e86e99fb0f7c1f2d2907e571fa3fd3a5fc19
Fabio Lenherr <fabio.lenherr@gmail.com>
2023-11-08 08:30:56 +0100

    fix: Move over pulse conversions

--- src/audio/audio.rs
+++ src/audio/audio.rs
@@ -2,6 +2,7 @@ use dbus::{
     arg::{self, Append, Arg, ArgType, Get},
     Signature,
 };
+use pulse::context::introspect::{SourceOutputInfo, SinkInputInfo, SinkInfo, SourceInfo};
 
 #[derive(Debug)]
 pub struct PulseError(&'static str);
@@ -50,6 +51,40 @@ impl Arg for Source {
     }
 }
 
+impl From<&SourceInfo<'_>> for Source {
+    fn from(value: &SourceInfo<'_>) -> Self {
+        let name_opt = &value.name;
+        let alias_opt = &value.description;
+        let name: String;
+        let alias: String;
+        if name_opt.is_none() {
+            name = String::from("");
+        } else {
+            name = String::from(name_opt.clone().unwrap());
+        }
+        if alias_opt.is_none() {
+            alias = String::from("");
+        } else {
+            alias = String::from(alias_opt.clone().unwrap());
+        }
+        let index = value.index;
+        let channels = value.channel_map.len() as u16;
+        let mut volume = vec![0; channels as usize];
+        for i in 0..channels as usize {
+            unsafe { *volume.get_unchecked_mut(i) = value.volume.get()[i].0 };
+        }
+        let muted = value.mute;
+        Self {
+            index,
+            name,
+            alias,
+            channels,
+            volume,
+            muted,
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Sink {
     index: u32,
@@ -92,6 +127,40 @@ impl Arg for Sink {
     const ARG_TYPE: arg::ArgType = ArgType::Struct;
     fn signature() -> Signature<'static> {
         unsafe { Signature::from_slice_unchecked("(ussqaub)\0") }
+    }
+}
+
+impl From<&SinkInfo<'_>> for Sink {
+    fn from(value: &SinkInfo<'_>) -> Self {
+        let name_opt = &value.name;
+        let alias_opt = &value.description;
+        let name: String;
+        let alias: String;
+        if name_opt.is_none() {
+            name = String::from("");
+        } else {
+            name = String::from(name_opt.clone().unwrap());
+        }
+        if alias_opt.is_none() {
+            alias = String::from("");
+        } else {
+            alias = String::from(alias_opt.clone().unwrap());
+        }
+        let index = value.index;
+        let channels = value.channel_map.len() as u16;
+        let mut volume = vec![0; channels as usize];
+        for i in 0..channels as usize {
+            unsafe { *volume.get_unchecked_mut(i) = value.volume.get()[i].0 };
+        }
+        let muted = value.mute;
+        Self {
+            index,
+            name,
+            alias,
+            channels,
+            volume,
+            muted,
+        }
     }
 }
 
@@ -142,6 +211,39 @@ impl Arg for InputStream {
     }
 }
 
+impl From<&SinkInputInfo<'_>> for InputStream {
+    fn from(value: &SinkInputInfo<'_>) -> Self {
+        let name_opt = &value.name;
+        let name: String;
+        if name_opt.is_none() {
+            name = String::from("");
+        } else {
+            name = String::from(name_opt.clone().unwrap());
+        }
+        let application_name = value
+            .proplist
+            .get_str("application.name")
+            .unwrap_or_default();
+        let sink_index = value.sink;
+        let index = value.index;
+        let channels = value.channel_map.len() as u16;
+        let mut volume = vec![0; channels as usize];
+        for i in 0..channels as usize {
+            unsafe { *volume.get_unchecked_mut(i) = value.volume.get()[i].0 };
+        }
+        let muted = value.mute;
+        Self {
+            index,
+            name,
+            application_name,
+            sink_index,
+            channels,
+            volume,
+            muted,
+        }
+    }
+}
+
 pub struct OutputStream {
     index: u32,
     name: String,
@@ -186,5 +288,38 @@ impl Arg for OutputStream {
     const ARG_TYPE: arg::ArgType = ArgType::Struct;
     fn signature() -> Signature<'static> {
         unsafe { Signature::from_slice_unchecked("(ussuqaub)\0") }
+    }
+}
+
+impl From<&SourceOutputInfo<'_>> for OutputStream {
+    fn from(value: &SourceOutputInfo<'_>) -> Self {
+        let name_opt = &value.name;
+        let name: String;
+        if name_opt.is_none() {
+            name = String::from("");
+        } else {
+            name = String::from(name_opt.clone().unwrap());
+        }
+        let application_name = value
+            .proplist
+            .get_str("application.name")
+            .unwrap_or_default();
+        let sink_index = value.source;
+        let index = value.index;
+        let channels = value.channel_map.len() as u16;
+        let mut volume = vec![0; channels as usize];
+        for i in 0..channels as usize {
+            unsafe { *volume.get_unchecked_mut(i) = value.volume.get()[i].0 };
+        }
+        let muted = value.mute;
+        Self {
+            index,
+            name,
+            application_name,
+            source_index: sink_index,
+            channels,
+            volume,
+            muted,
+        }
     }
 }

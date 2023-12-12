906b5ddb17bfd0ce3d56405d34e493a772f4f058
takotori <tranfelix99@gmail.com>
2023-12-12 17:23:07 +0100

    print stuff

--- src/network/connection.rs
+++ src/network/connection.rs
@@ -1084,10 +1084,8 @@ fn get_addresses(map: &PropMap, address_type: &'static str) -> Vec<Address> {
 
     for t in test.unwrap().0.as_iter().unwrap() {
         dbg!(t);
-        dbg!(t.signature());
         t.as_iter().unwrap().for_each(|x| {
             dbg!(x);
-            dbg!(x.signature());
         });
 
         let a = t.box_clone();

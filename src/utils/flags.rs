e07a4208d5c5d7c2264da4b95ac2ba031bd33bd2
DashieTM <fabio.lenherr@gmail.com>
2024-04-21 14:09:05 +0200

    cli_flags: Add cli flags as global

--- src/utils/flags.rs
+++ src/utils/flags.rs
@@ -1,12 +1,21 @@
+use crate::parse_flags;
+
 use super::variant::Variant;
+use once_cell::sync::Lazy;
 
 // Handles all command line flags
 #[derive(Debug)]
-pub enum Flag<'a> {
-    ConfigDir(&'a String),
-    PluginDir(&'a String),
+pub enum Flag {
+    ConfigDir(String),
+    PluginDir(String),
     Other((String, Variant)),
 }
 
+#[allow(clippy::declare_interior_mutable_const)]
+pub const FLAGS: Lazy<Flags> = Lazy::new(|| {
+    let flags = std::env::args().collect::<Vec<String>>();
+    parse_flags(flags)
+});
+
 #[derive(Debug)]
-pub struct Flags<'a>(pub Vec<Flag<'a>>);
+pub struct Flags(pub Vec<Flag>);

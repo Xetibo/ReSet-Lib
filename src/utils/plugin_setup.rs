fb05f2affcb0e8df21382dd64ca289abbd58c287
DashieTM <fabio.lenherr@gmail.com>
2024-05-30 21:15:35 +0200

    plugin: debug print plugin path

--- src/utils/plugin_setup.rs
+++ src/utils/plugin_setup.rs
@@ -99,6 +99,7 @@ static SETUP_LIBS: fn() = || {
         if config.is_empty() {
             SETUP_PLUGIN_DIR()
         } else {
+            dbg!(&config);
             let maybe_dir = PathBuf::from(config.to_string());
             if maybe_dir.is_dir() {
                 Some(maybe_dir)
